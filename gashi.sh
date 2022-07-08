3884f29fae2689f917e9c0c2d3abafc5131a09bf
ngel4ng <108847948+ngel4ng@users.noreply.github.com>
2022-07-08 12:52:26 -0700

    Update gashi.sh

--- gashi.sh
+++ gashi.sh
@@ -1,1 +1,6 @@
-
+pip install colabcode
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip -o ngrok-stable-linux-amd64.zip
+./ngrok authtoken 2Ba9NSUGF6tL3Kkuy0hQhosi7Gf_2kDbhXA4rkcQgLBvpd1u8
+chmod +x go.py
+./go.py
